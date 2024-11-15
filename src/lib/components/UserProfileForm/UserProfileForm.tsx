e7238e297dea646b3858ecc5c55610b7a54bfb6d
shlomo glikman <shlomog@capitolis.com>
2024-11-15 12:56:28 +0200

    update UserEditProfileForm with more fields

--- src/lib/components/UserProfileForm/UserProfileForm.tsx
+++ src/lib/components/UserProfileForm/UserProfileForm.tsx
@@ -1,25 +1,36 @@
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
-import { FormProvider, TextFieldElement, useForm } from 'react-hook-form-mui';
-import { DISPLAY_TEXTS } from '@/lib/consts/displayTexts';
-
+import {
+  FormProvider,
+  TextareaAutosizeElement,
+  TextFieldElement,
+  useForm,
+} from 'react-hook-form-mui';
+import { DISPLAY_TEXTS, ILang } from '@/lib/consts/displayTexts';
+import Typography from '@mui/material/Typography';
 enum EUserProfileFields {
   Name = 'name',
   MainEmail = 'mainEmail',
+  MainPhone = 'mainPhone',
+  Comments = 'comments',
 }
 
-const fields = {
-  [EUserProfileFields.Name]: {
-    label: {
-      he: 'שם',
-      en: 'Name',
-    },
+const formTitle: Record<ILang, string> = {
+  he: 'פרופיל משתמש',
+  en: 'User Profile',
+};
+const fields: Record<ILang, Record<EUserProfileFields, string>> = {
+  he: {
+    [EUserProfileFields.Name]: 'שם',
+    [EUserProfileFields.MainEmail]: 'אימייל',
+    [EUserProfileFields.MainPhone]: 'טלפון',
+    [EUserProfileFields.Comments]: 'הערות',
   },
-  [EUserProfileFields.MainEmail]: {
-    label: {
-      he: 'אימייל',
-      en: 'Email',
-    },
+  en: {
+    [EUserProfileFields.Name]: 'Name',
+    [EUserProfileFields.MainEmail]: 'Email',
+    [EUserProfileFields.MainPhone]: 'Phone',
+    [EUserProfileFields.Comments]: 'Comments',
   },
 };
 
@@ -28,6 +39,7 @@ export const UserProfileForm = () => {
     defaultValues: {
       [EUserProfileFields.Name]: '',
       [EUserProfileFields.MainEmail]: '',
+      [EUserProfileFields.MainPhone]: '',
     },
   });
   const handeLogin = form.handleSubmit(async (values) => {
@@ -37,16 +49,27 @@ export const UserProfileForm = () => {
     <FormProvider {...form}>
       <form onSubmit={handeLogin}>
         <Stack sx={{ mt: 2 }} spacing={2} alignItems={'center'}>
+          <Typography variant='h5'>{formTitle.he}</Typography>
           <TextFieldElement
-            label={fields.name.label.he}
+            label={fields.he[EUserProfileFields.Name]}
             name={EUserProfileFields.Name}
             required
           />
           <TextFieldElement
             type='email'
-            label={fields.mainEmail.label.he}
+            label={fields.he[EUserProfileFields.MainEmail]}
             name={EUserProfileFields.MainEmail}
             required
+          />
+          <TextFieldElement
+            type='tel'
+            label={fields.he[EUserProfileFields.MainPhone]}
+            name={EUserProfileFields.MainPhone}
+            required
+          />
+          <TextareaAutosizeElement
+            name={EUserProfileFields.Comments}
+            placeholder={fields.he[EUserProfileFields.Comments]}
           />
           <Button type='submit'>{DISPLAY_TEXTS.he.buttons.save}</Button>
         </Stack>
